a4056371a30caad8b4205353e223a477b556e515
Bruno Carlini <bruno-carlini@hotmail.com>
2022-09-17 01:03:45 -0300

    CRUD Finalizado

--- src/tarefa/controllers/tarefa.controllers.ts
+++ src/tarefa/controllers/tarefa.controllers.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Body, Put, Delete } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 import { Tarefa } from "../entities/tarefa.entity";
 import { TarefaService } from "../service/tarefa.service";
 
+@ApiTags('Tarefa')
 @Controller('/tarefa')
 export class TarefaController {
     constructor(private readonly service: TarefaService){}
